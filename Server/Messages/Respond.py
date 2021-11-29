bae650287148d13f3e1fc471405ac4c2f887f18f
Ayman <ayman.khawaleda@gmail.com>
2021-11-29 15:09:07 +0200

    Messages and Respond Are Done

---
+++ Server/Messages/Respond.py
@@ -0,0 +1,15 @@
+from Messages import Message as Ms
+
+
+class RespondMessage(Ms.Message):
+    def __init__(self, details: str, message: dict = None):
+        try:
+            if message is None:
+                self.message_info = {
+                    "Type": "Respond",
+                    "Details": details
+                }
+            elif message["Type"] == "Respond":
+                super(RespondMessage, self).__init__(message=message)
+        except Exception as e:
+            print(e)
