5b35c5bce6fadab799333c35cd722b52d6128e6d
Ayman Alkhawaleda <ayman.khawaleda@gmail.com>
2021-12-11 07:36:49 +0200

    Correct main loop

--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,4 @@
 import asyncio
-from Messages import Respond
 from controller import ServerController as sc
 
 
@@ -8,9 +7,15 @@ async def main():
     asyncio.create_task(task)
 
 
+def exception_handler(loop: asyncio.AbstractEventLoop, dic: dict):
+    pass
+
+
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
+        loop = asyncio.get_event_loop()
+        loop.set_exception_handler(exception_handler)
+        loop.run_until_complete(main())
     except KeyboardInterrupt as e:
         print("Server Shout Down")
     except RuntimeError as e:
